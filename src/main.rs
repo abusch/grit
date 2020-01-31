e1b1a7c3c6481d2a4f5b36aef30b0b7ed76a1e97
Antoine Büsch <antoine.busch@gmail.com>
2020-01-31 13:34:30 +1100

    Split screen drawing into its own module

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,18 @@
-use chrono::{offset::FixedOffset, TimeZone};
 use crossterm::{
     cursor,
     event::KeyCode,
     queue,
-    style::{Attribute, Color::*},
     terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use git2::{Commit, Repository, RepositoryState, Sort};
-use lazy_static::lazy_static;
+use git2::Repository;
 use std::error::Error;
 use std::io::Write;
-use termimad::{
-    ansi, Alignment, Area, CompoundStyle, Event, EventSource, ListView, ListViewCell,
-    ListViewColumn, MadSkin,
-};
+use termimad::{Event, EventSource};
 
-// mod screen;
-//
+use screen::Screen;
+
+mod screen;
+
 const UP: Event = Event::simple_key(KeyCode::Up);
 const DOWN: Event = Event::simple_key(KeyCode::Down);
 const PAGE_UP: Event = Event::simple_key(KeyCode::PageUp);
@@ -25,9 +21,7 @@ const HOME: Event = Event::simple_key(KeyCode::Home);
 const END: Event = Event::simple_key(KeyCode::End);
 const ESC: Event = Event::simple_key(KeyCode::Esc);
 
-lazy_static! {
-    static ref SKIN: MadSkin = make_skin();
-}
+pub type BoxError = Box<dyn Error + Send + Sync>;
 
 fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     let mut w = std::io::stderr();
@@ -41,107 +35,23 @@ fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
 
     let pwd = std::env::current_dir()?;
     let repo = Repository::discover(pwd)?;
-    let columns = vec![
-        ListViewColumn::new(
-            "commit date",
-            6,
-            26,
-            Box::new(|t: &Commit| {
-                let when = t.author().when();
-                let offset = FixedOffset::east(when.offset_minutes() * 60);
-                let date_time = offset.timestamp(when.seconds(), 0);
-                ListViewCell::new(date_time.to_string(), &SKIN.paragraph.compound_style)
-            }),
-        ),
-        ListViewColumn::new(
-            "author",
-            6,
-            20,
-            Box::new(|t: &Commit| {
-                ListViewCell::new(
-                    t.author()
-                        .name()
-                        .unwrap_or_else(|| "<invalid utf8>")
-                        .to_string(),
-                    &SKIN.paragraph.compound_style,
-                )
-            }),
-        ),
-        ListViewColumn::new(
-            "message",
-            6,
-            120,
-            Box::new(|t: &Commit| {
-                ListViewCell::new(
-                    t.summary().unwrap().to_string(),
-                    &SKIN.paragraph.compound_style,
-                )
-            }),
-        )
-        .with_align(Alignment::Left),
-    ];
 
-    let (width, height) = terminal::size()?;
-    let title_area = Area::new(0, 0, width, 1);
-    let list_area = Area::new(0, 1, width, height - 2);
-    let mut commit_list = ListView::new(list_area, columns, &SKIN);
-
-    let state = match repo.state() {
-        RepositoryState::Clean => "".to_string(),
-        s => format!("{:?}", s),
-    };
-
-    SKIN.write_in_area_on(
-        &mut w,
-        &format!(
-            "# **{}**  *{}*",
-            repo.workdir().unwrap_or_else(|| repo.path()).display(),
-            state
-        ),
-        &title_area,
-    )?;
-    // println!("State: {:?}", repo.state());
-    // if let Some(path) = repo.workdir() {
-    //     println!("Workdir: {}", path.display());
-    // } else {
-    //     println!("Bare repo");
-    // }
-
-    let mut revwalk = repo.revwalk().unwrap();
-    revwalk.set_sorting(Sort::TOPOLOGICAL);
-    revwalk.push_head().unwrap();
-    for oid in revwalk {
-        let oid = oid.unwrap();
-        let commit = repo.find_commit(oid).unwrap();
-        commit_list.add_row(commit);
-    }
-    commit_list.select_first_line();
-    commit_list.update_dimensions();
-
-    // let statuses = repo.statuses(None).unwrap();
-
-    // for status_entry in statuses.iter() {
-    //     println!(
-    //         "{:?} {}",
-    //         status_entry.status(),
-    //         status_entry.path().unwrap()
-    //     );
-    // }
+    let mut screen = Screen::new(repo)?;
     loop {
         let mut quit = false;
-        commit_list.write_on(&mut w)?;
+        screen.display(&mut w)?;
         if let Ok(event) = rx.recv() {
             match event {
-                UP => commit_list.try_select_next(true),
-                DOWN => commit_list.try_select_next(false),
-                PAGE_UP => commit_list.try_scroll_pages(-1),
-                PAGE_DOWN => commit_list.try_scroll_pages(1),
-                HOME => commit_list.select_first_line(),
-                END => commit_list.select_last_line(),
+                UP => screen.commit_list.try_select_next(true),
+                DOWN => screen.commit_list.try_select_next(false),
+                PAGE_UP => screen.commit_list.try_scroll_pages(-1),
+                PAGE_DOWN => screen.commit_list.try_scroll_pages(1),
+                HOME => screen.commit_list.select_first_line(),
+                END => screen.commit_list.select_last_line(),
                 Event::Resize(w, h) => {
-                    commit_list.area.width = w;
-                    commit_list.area.height = h;
-                    commit_list.update_dimensions();
+                    screen.commit_list.area.width = w;
+                    screen.commit_list.area.height = h;
+                    screen.commit_list.update_dimensions();
                 }
                 ESC => quit = true,
                 _ => (),
@@ -159,13 +69,4 @@ fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     w.flush()?;
 
     Ok(())
-}
-
-fn make_skin() -> MadSkin {
-    let mut skin = MadSkin::default();
-    skin.headers[0].compound_style = CompoundStyle::with_attr(Attribute::Bold);
-    skin.headers[0].align = Alignment::Left;
-    skin.italic.set_fg(ansi(225));
-    skin.bold = CompoundStyle::with_fg(Blue);
-    skin
 }
---
+++ src/screen.rs
@@ -0,0 +1,147 @@
+use crate::BoxError;
+
+use std::io::Write;
+
+use chrono::{offset::FixedOffset, DateTime, TimeZone};
+use crossterm::{
+    queue,
+    style::{Attribute, Color::*},
+    terminal::{self, Clear, ClearType},
+};
+use git2::{Commit, Repository, RepositoryState, Sort};
+use lazy_static::lazy_static;
+use termimad::{
+    ansi, Alignment, Area, CompoundStyle, ListView, ListViewCell, ListViewColumn, MadSkin,
+};
+
+pub struct CommitInfo {
+    pub time: DateTime<FixedOffset>,
+    pub author: String,
+    pub message: String,
+}
+
+impl CommitInfo {
+    pub fn new(commit: Commit) -> Self {
+        let when = commit.author().when();
+        let offset = FixedOffset::east(when.offset_minutes() * 60);
+        let date_time = offset.timestamp(when.seconds(), 0);
+        Self {
+            time: date_time,
+            author: commit
+                .author()
+                .name()
+                .unwrap_or_else(|| "<invalid utf8>")
+                .to_string(),
+            message: commit
+                .summary()
+                .unwrap_or_else(|| "<invalid utf8>")
+                .to_string(),
+        }
+    }
+}
+
+lazy_static! {
+    static ref SKIN: MadSkin = make_skin();
+}
+
+pub struct Screen<'t> {
+    repo: Repository,
+    pub commit_list: ListView<'t, CommitInfo>,
+    dimensions: (u16, u16),
+    skin: &'t MadSkin,
+}
+
+impl<'t> Screen<'t> {
+    pub fn new(repo: Repository) -> Result<Self, BoxError> {
+        let columns = vec![
+            ListViewColumn::new(
+                "commit date",
+                6,
+                26,
+                Box::new(|t: &CommitInfo| {
+                    ListViewCell::new(t.time.to_string(), &SKIN.paragraph.compound_style)
+                }),
+            ),
+            ListViewColumn::new(
+                "author",
+                6,
+                20,
+                Box::new(|t: &CommitInfo| {
+                    ListViewCell::new(t.author.clone(), &SKIN.paragraph.compound_style)
+                }),
+            ),
+            ListViewColumn::new(
+                "message",
+                6,
+                120,
+                Box::new(|t: &CommitInfo| {
+                    ListViewCell::new(t.message.clone(), &SKIN.paragraph.compound_style)
+                }),
+            )
+            .with_align(Alignment::Left),
+        ];
+
+        let (width, height) = terminal::size()?;
+        let list_area = Area::new(0, 1, width, height - 2);
+        let mut commit_list = ListView::new(list_area, columns, &SKIN);
+
+        let mut revwalk = repo.revwalk().unwrap();
+        revwalk.set_sorting(Sort::TOPOLOGICAL);
+        revwalk.push_head().unwrap();
+        for oid in revwalk {
+            let oid = oid.unwrap();
+            let commit = repo.find_commit(oid).unwrap();
+            commit_list.add_row(CommitInfo::new(commit));
+        }
+        // commit_list.select_first_line();
+        commit_list.update_dimensions();
+
+        Ok(Self {
+            repo,
+            commit_list,
+            skin: &SKIN,
+            dimensions: (width, height),
+        })
+    }
+
+    pub fn display<W: Write>(&mut self, w: &mut W) -> Result<(), BoxError> {
+        let (width, height) = terminal::size()?;
+        if (width, height) != self.dimensions {
+            queue!(w, Clear(ClearType::All))?;
+            self.commit_list.area.width = width;
+            self.commit_list.area.height = height - 2;
+            self.commit_list.update_dimensions();
+        }
+
+        let title_area = Area::new(0, 0, width, 1);
+        let state = match self.repo.state() {
+            RepositoryState::Clean => "".to_string(),
+            s => format!("{:?}", s),
+        };
+
+        self.skin.write_in_area_on(
+            w,
+            &format!(
+                "# **{}**  *{}*",
+                self.repo
+                    .workdir()
+                    .unwrap_or_else(|| self.repo.path())
+                    .display(),
+                state
+            ),
+            &title_area,
+        )?;
+        self.commit_list.write_on(w)?;
+
+        Ok(())
+    }
+}
+
+fn make_skin() -> MadSkin {
+    let mut skin = MadSkin::default();
+    skin.headers[0].compound_style = CompoundStyle::with_attr(Attribute::Bold);
+    skin.headers[0].align = Alignment::Left;
+    skin.italic.set_fg(ansi(225));
+    skin.bold = CompoundStyle::with_fg(Blue);
+    skin
+}
